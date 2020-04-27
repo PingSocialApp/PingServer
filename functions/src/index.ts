46ef8a79d327a69e536cf67ebed85846b33cec62
Sree Grandhe <sreegrandhe@gmail.com>
2020-04-27 01:20:24 -0500

    Implement New User Document
    On Auth Create Trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,12 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+const serviceAccount = require('../circles-4d081-firebase-adminsdk-rtjsi-51616d71b7.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+});
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +14,41 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const newUser = functions.auth.user().onCreate((user) => {
+    let seed = Math.floor(Math.random() * Math.floor(10000));
+    admin.firestore().doc('users/' + user.uid).set({
+        name: user.displayName,
+        bio: 'Just Chilling',
+        facebookID: '',
+        instagramID: '',
+        linkedinID: '',
+        numberID: '',
+        personalEmailID: '',
+        snapchatID: '',
+        tiktokID: '',
+        twitterID: '',
+        venmoID: '',
+        websiteID: '',
+        sendingMessage: [
+            'Call me',
+            'Urgent',
+            'Missing you',
+            'What\'s up',
+            'Text me'
+        ],
+        responseMessage: [
+            'Okay',
+            'No',
+            'Me too',
+            'Working',
+            'Having fun'
+        ],
+        profilepic: 'https://picsum.photos/seed/' + seed + '/300'
+    }).then(result => {
+        console.log('New User Created');
+    }).catch(e => {
+        console.log(e);
+    });
+});
+
